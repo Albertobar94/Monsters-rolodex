6a067adcbd1695ee1379d9d4d5eaa7c4209fe46e
DevAlberto <devalbertobarboza@gmail.com>
2021-01-15 17:36:00 -0300

    added data from API and got names for the monsters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    }
+
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => Response.json())
+        .then( users => this.setState({ monsters: users }))
+    }
+    
+
+  }
+
+
+  render() (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        this.state.map( monster =>  <h1 key={monster.id}> {monster.name} </h1> )
+      }
     </div>
   );
 }
